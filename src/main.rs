beb66ca396fc0bd9074d55747506480ce0e1908c
metarmask <ihvarfner@gmail.com>
2026-02-08 19:50:28 +0100

    Fix logic and reliability issues

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,34 @@ struct Args {
     idle: u64,
 }
 
+impl App {
+    fn new(args: Args) -> Result<App> {
+        let shutdown = Arc::new(AtomicBool::new(false));
+        flag::register(SIGTERM, Arc::clone(&shutdown))?;
+        flag::register(SIGINT, Arc::clone(&shutdown))?;
+        flag::register(SIGHUP, Arc::clone(&shutdown))?;
+        Ok(App {
+            shutdown,
+            idle_timeout: Duration::from_secs(args.idle),
+            args,
+            process: None,
+            midi: None,
+            volume: Volume::new(&xdg::BaseDirectories::with_prefix("midi-start-program"))?,
+        })
+    }
+}
+
+fn main() -> Result<()> {
+    let mut app = App::new(Args::parse())?;
+    println!("stdout");
+    eprintln!("stderr");
+    loop {
+        if !app.tick_main()? {
+            return Ok(());
+        }
+    }
+}
+
 #[derive(Error, Debug)]
 pub enum MidiStartPrgError {
     #[error("somethingy io-y {0}")]
@@ -79,7 +107,7 @@ pub enum MidiStartPrgError {
 
 type Result<T, E = MidiStartPrgError> = StdResult<T, E>;
 
-fn pause_media() -> Result<(), MidiStartPrgError> {
+fn pause_media() -> Result<()> {
     let output = Command::new("playerctl").args(["pause"]).output()?;
     if output.status.success() {
         return Ok(());
@@ -98,16 +126,14 @@ fn pause_media() -> Result<(), MidiStartPrgError> {
     Ok(())
 }
 
-fn systemctl_suspend() -> Result<(), MidiStartPrgError> {
+fn systemctl_suspend() -> Result<()> {
     // TODO: This is to make sure messages reach the piano before, maybe make it less arbitrary
     sleep(Duration::from_millis(500));
     let status = Command::new("systemctl")
         .args(["suspend", "--check-inhibitors=no"])
         .status()?;
-    if !status.success() {
-        println!("systemctl suspend failed (ignored)");
-    }
-    Ok(())
+
+    status.exit_ok().map_err(From::from)
 }
 
 struct PianoteqProcess {
@@ -117,7 +143,7 @@ struct PianoteqProcess {
 }
 
 impl PianoteqProcess {
-    fn new(executable: &str) -> Result<Self, MidiStartPrgError> {
+    fn new(executable: &str) -> Result<Self> {
         let child = Command::new(executable).spawn()?;
         Ok(PianoteqProcess {
             child,
@@ -131,11 +157,11 @@ impl PianoteqProcess {
         })
     }
 
-    fn terminate(&mut self) -> Result<(), MidiStartPrgError> {
+    fn terminate(&mut self) -> Result<()> {
         self.child.send_signal(15).map_err(Into::into)
     }
 
-    fn check_has_exited(&mut self) -> Result<bool, MidiStartPrgError> {
+    fn check_has_exited(&mut self) -> Result<bool> {
         if self.has_exited {
             return Ok(true);
         }
@@ -166,9 +192,6 @@ struct App {
     args: Args,
     shutdown: Arc<AtomicBool>,
     process: Option<PianoteqProcess>,
-    last_note_instant: Instant,
-    last_msg_instant: Instant,
-    rightmost_count: u8,
     midi: Option<MidiConnection>,
     volume: Volume,
     idle_timeout: Duration,
@@ -179,48 +202,58 @@ struct MidiConnection {
     /// Closes connection when dropped
     _midi_in: MidiInputConnection<()>,
     midi_in_rx: Receiver<Result<MidiMsg, midi_msg::ParseError>>,
+    last_note_instant: Option<Instant>,
+    last_update_instant: Instant,
+    rightmost_count: u8,
 }
 
-impl App {
-    fn tick_midi(&mut self, msg: MidiMsg) -> Result<(), MidiStartPrgError> {
+enum MidiTickAction {
+    StartProcess,
+    Suspend,
+}
+
+impl MidiConnection {
+    fn tick_midi(&mut self, msg: MidiMsg) -> Result<Option<MidiTickAction>> {
         const RIGHTMOST_C: u8 = 108;
         const TRIGGER_NOTE: u8 = RIGHTMOST_C - 2;
 
-        self.last_msg_instant = Instant::now();
+        self.last_update_instant = Instant::now();
         #[rustfmt::skip]
         let ChannelVoice { msg: NoteOn { note, velocity, .. }, .. } = msg else {
-            return Ok(());
+            return Ok(None);
         };
+        self.last_note_instant = Some(Instant::now());
         if velocity > 0 {
-            if note == TRIGGER_NOTE && self.rightmost_count >= 7 {
-                return self.suspend();
+            if note == TRIGGER_NOTE && self.rightmost_count >= 5 {
+                self.rightmost_count = 0;
+                return Ok(Some(MidiTickAction::Suspend));
             } else if note == RIGHTMOST_C {
                 self.rightmost_count = self.rightmost_count.saturating_add(1);
             } else {
                 self.rightmost_count = 0;
             }
         }
-        self.last_note_instant = Instant::now();
-        self.start_process()
+        Ok(Some(MidiTickAction::StartProcess))
     }
+}
 
-    fn start_chime(&mut self) -> Result<(), MidiStartPrgError> {
-        const START_NOTE: u8 = 60; // middle C
+impl App {
+    fn touch_note(midi_out: &mut MidiOut, note: u8) -> Result<()> {
         const START_VELOCITY: u8 = 80;
-        if let Some(Some(midi_out)) = self.midi.as_mut().map(|a| a.midi_out.as_mut()) {
-            midi_out.set_local_control(false)?;
-            midi_out.send_note(START_NOTE, START_VELOCITY, true)?;
-            midi_out.send_note(START_NOTE, START_VELOCITY, false)?;
-        }
+        midi_out.send_note(note, START_VELOCITY, true)?;
+        midi_out.send_note(note, START_VELOCITY, false)?;
         Ok(())
     }
 
-    fn start_process(&mut self) -> Result<(), MidiStartPrgError> {
+    fn start_process(&mut self) -> Result<()> {
         if self.process.is_some() {
             return Ok(());
         }
-        let start_result = (|| -> Result<(), MidiStartPrgError> {
-            self.start_chime()?;
+        let start_result = (|| -> Result<()> {
+            if let Some(Some(midi_out)) = self.midi.as_mut().map(|a| a.midi_out.as_mut()) {
+                Self::touch_note(midi_out, 60)?;
+                midi_out.set_local_control(false)?;
+            }
             if pause_media().is_ok() {
                 self.volume.set_swapped(true)?;
             } else {
@@ -236,35 +269,52 @@ impl App {
         Ok(())
     }
 
-    fn maybe_cleanup_process(&mut self) -> Result<(), MidiStartPrgError> {
+    fn maybe_cleanup_process(&mut self) -> Result<()> {
         if let Some(process) = &mut self.process
             && process.check_has_exited()?
         {
             self.process = None;
-            self.do_process_cleanup()?;
         };
+        // Separate scope so that `process` and its KeepAwake is dropped earlier
+        if self.process.is_none() {
+            self.do_process_cleanup()?;
+        }
         Ok(())
     }
 
-    fn do_process_cleanup(&mut self) -> Result<(), MidiStartPrgError> {
+    fn do_process_cleanup(&mut self) -> Result<()> {
         let volume_swap = self.volume.set_swapped(false);
-        let local_control_setting =
-            if let Some(Some(midi_out)) = self.midi.as_mut().map(|a| a.midi_out.as_mut()) {
+        let local_control_setting = if let Some(midi) = self.midi.as_mut() {
+            midi.last_note_instant = None;
+            if let Some(midi_out) = &mut midi.midi_out {
                 midi_out.set_local_control(true).map_err(From::from)
             } else {
                 Ok(())
-            };
+            }
+        } else {
+            Ok(())
+        };
         volume_swap.and(local_control_setting)
+    }
+
+    fn is_idle(&self) -> bool {
+        self.midi.as_ref().is_some_and(|conn| {
+            if let Some(last_note_instant) = conn.last_note_instant {
+                last_note_instant.elapsed() > self.idle_timeout
+                    && conn.last_update_instant.elapsed() < Duration::from_millis(1000)
+            } else {
+                false
+            }
+        })
     }
 
     fn tick_main(&mut self) -> Result<bool> {
         self.maybe_cleanup_process()?;
 
         let is_shutdown_requested = self.shutdown.load(Ordering::Relaxed);
-        let is_idle = self.last_note_instant.elapsed() > self.idle_timeout;
-        if is_shutdown_requested || is_idle {
+        if is_shutdown_requested || self.is_idle() {
             self.terminate_and_wait()?;
-            if is_idle {
+            if self.is_idle() {
                 self.suspend()?;
             }
             return Ok(!is_shutdown_requested);
@@ -273,29 +323,28 @@ impl App {
         if let Err(err) = self.start_midi_if_needed() {
             if let MidiStartPrgError::NoMatchingMidiPort = err {
                 eprintln!("{}", err);
+                sleep(Duration::from_millis(400));
                 return Ok(true);
             }
             return Err(err);
         }
         #[rustfmt::skip]
         let midi = self.midi.as_mut().expect("start_midi_if_needed initialized it on success");
-        match midi.midi_in_rx.recv_timeout(Duration::from_secs(3)) {
+        match midi.midi_in_rx.recv_timeout(Duration::from_secs(10)) {
             Ok(channel_message) => match channel_message {
-                Ok(midi_message) => {
-                    self.tick_midi(midi_message)?;
-                }
+                Ok(midi_message) => match midi.tick_midi(midi_message)? {
+                    Some(MidiTickAction::StartProcess) => self.start_process()?,
+                    Some(MidiTickAction::Suspend) => self.suspend()?,
+                    None => {}
+                },
                 Err(parse_error) => {
                     println!("MIDI parse error: {}", parse_error);
                 }
             },
             Err(receive_error) => match receive_error {
-                RecvTimeoutError::Timeout => {
-                    if self.last_msg_instant.elapsed() > Duration::from_secs(10) {
-                        println!("midi idle timeout");
-                    }
-                }
-                RecvTimeoutError::Disconnected => {
-                    println!("midi input thread disconnected");
+                reason @ (RecvTimeoutError::Timeout | RecvTimeoutError::Disconnected) => {
+                    println!("MIDI listening failed: {}", reason);
+                    self.midi = None;
                 }
             },
         }
@@ -303,13 +352,21 @@ impl App {
     }
 
     fn suspend(&mut self) -> Result<()> {
-        self.rightmost_count = 0;
         self.midi = None;
         self.terminate_and_wait()?;
-        systemctl_suspend()
+        match systemctl_suspend() {
+            Err(MidiStartPrgError::ExitStatusError(error)) => {
+                eprintln!(
+                    "Ignoring failed suspend, error code {}",
+                    error.code().unwrap_or_default()
+                );
+            }
+            other => other?,
+        }
+        Ok(())
     }
 
-    fn terminate_and_wait(&mut self) -> Result<(), MidiStartPrgError> {
+    fn terminate_and_wait(&mut self) -> Result<()> {
         if let Some(process) = self.process.as_mut() {
             process.terminate()?;
         }
@@ -317,9 +374,10 @@ impl App {
             && !process.check_has_exited()?
         {
             println!("waiting for Pianoteq to exit");
-            sleep(Duration::from_millis(100));
             self.maybe_cleanup_process()?;
+            sleep(Duration::from_millis(400));
         }
+        self.maybe_cleanup_process()?;
         Ok(())
     }
 
@@ -345,8 +403,6 @@ impl App {
         println!("connecting to {}", port_name);
         let mut ctx = ReceiverContext::new();
         let (tx, rx) = channel();
-        self.last_msg_instant = Instant::now();
-        self.rightmost_count = 0;
         let midi_out = MidiOut::open(&self.args.device_prefix)?;
         if midi_out.is_none() {
             println!("no MIDI output port found");
@@ -360,37 +416,15 @@ impl App {
             },
             (),
         )?;
-
-        println!("connected");
+        println!("Connected.");
         self.midi = Some(MidiConnection {
             _midi_in,
             midi_out,
             midi_in_rx: rx,
+            last_note_instant: None,
+            last_update_instant: Instant::now(),
+            rightmost_count: 0,
         });
         Ok(())
-    }
-}
-
-fn main() -> Result<(), MidiStartPrgError> {
-    let shutdown = Arc::new(AtomicBool::new(false));
-    flag::register(SIGTERM, Arc::clone(&shutdown))?;
-    flag::register(SIGINT, Arc::clone(&shutdown))?;
-    flag::register(SIGHUP, Arc::clone(&shutdown))?;
-    let args = Args::parse();
-    let mut app = App {
-        shutdown,
-        idle_timeout: Duration::from_secs(args.idle),
-        args,
-        process: None,
-        last_note_instant: Instant::now(),
-        last_msg_instant: Instant::now(),
-        rightmost_count: 0,
-        midi: None,
-        volume: Volume::new(&xdg::BaseDirectories::with_prefix("midi-start-program"))?,
-    };
-    loop {
-        if !app.tick_main()? {
-            return Ok(());
-        }
     }
 }
